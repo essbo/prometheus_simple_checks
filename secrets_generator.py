9b1262219879291e4d9429359af9e78d07b855b8
Bosse "essbo" Klein <73748381+essbo@users.noreply.github.com>
2023-11-15 14:38:19 +0100

    Update and rename create_secrets_aesterisk.py to secrets_generator.py

--- secrets_generator.py
+++ secrets_generator.py
@@ -2,6 +2,7 @@
 import pickle
 import base64
 # Theese Values are default values and are ment to be changed
+# Edit this DICT with the Values needed for the Secrets File inside the Script
 
 dicts = {
         "server": "",
